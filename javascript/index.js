196220d818558cb4e33687c53d82c8536501478c
Ayesha <42050299+Ayesha-Steenhuisen@users.noreply.github.com>
2021-12-20 23:53:20 +0100

    js excercise 1

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+console.log("Hello, Winc Academy")
+
+let name = "Ayesha"
+console.log(name)
+
+// let if = "hello?" // doesn't work, if is a different keyword
+
+let num1 = 4
+let num2 = 4
+console.log(num1 + num2)
+
+let str1 = "4"
+let str2 = "4"
+console.log(str1 + str2)
+
+num2 = 7
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+age = 200
+agestr = "200"
+console.log(typeof age)
+console.log(typeof agestr)
