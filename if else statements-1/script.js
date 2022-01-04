06faadfabc3608351df2c30439fd8be51f60f6e5
Ayesha <42050299+Ayesha-Steenhuisen@users.noreply.github.com>
2022-01-04 21:56:09 +0100

    if else part 1
    part one

---
+++ if else statements-1/script.js
@@ -0,0 +1,28 @@
+const age = 0;
+
+if (age >= 18) {
+    // greater then or equal, denk aan uitspraak
+    console.log("age is appropriate");
+} else {
+    console.log("not valid age");
+}
+
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Sure, you can come in");
+} else {
+    console.log("not a valid sex");
+}
+
+const driverStatus = "designated";
+
+if (driverStatus === "designated") {
+    console.log("allowed to drive");
+} else {
+    console.log("please keep walking");
+}
+
+
+
