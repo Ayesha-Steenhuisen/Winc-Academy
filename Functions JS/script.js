2707f0e8784d9f54605c02f30b1f0274aa2025b0
Ayesha <42050299+Ayesha-Steenhuisen@users.noreply.github.com>
2022-01-04 23:49:15 +0100

    functions consoling

---
+++ Functions JS/script.js
@@ -0,0 +1,47 @@
+function pizza() {
+    console.log("Make dough");
+    console.log("Make Sauce");
+    console.log("Add some of that cheesy goodgood");
+}
+
+// pizza();
+// pizza();
+// /* whooo */ pizza();
+
+function sushi() {
+    console.log("roll rice");
+    console.log("put in veggies and eal");
+    console.log("roll it up well");
+    console.log("add sesame, wasabi and soy")
+}
+// sushi();
+// sushi();
+// sushi();
+console.log("i want 3 sushi an two pizzas")
+sushi();
+sushi();
+sushi();
+pizza();
+pizza();
+// Function basics: exercise
+// In this exercise we're going to write our first functions. Introduction
+
+// We're going to write a function that makes pizza (sadly, we're not going to actually make pizza).
+
+// create a new JavaScript file
+// first just check if you can run it with node
+// to take a step in the pizza-making process print the step to the console
+// think of a good clear name for your function first
+// think of at least three steps you need in your function
+// write the function
+// run the code, are you getting the result you expect? Why (not)?
+// now add code to call your function
+// now call your function three times
+// Now we have a single function, but let's make another function! We also want to make sushi.
+
+// continue in the same JavaScript file
+// add a function that makes sushi, use a good name
+// call this function, check that it works
+// So now, let's for a minute pretend that we work in a restaurant and we get an order for three meals of sushi and two pizzas.
+
+// change your code so that the output of your program is the steps for making sushi, three times and pizza two times
